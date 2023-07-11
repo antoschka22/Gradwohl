1b15b50ee08abd890a996c8d34aab195623ee526
antoschka22 <antoniomolina2207@gmail.com>
2023-07-11 19:45:03 +0200

    docker settings

--- backend/spring/src/main/java/at/gradwohl/website/WebsiteApplication.java
+++ backend/spring/src/main/java/at/gradwohl/website/WebsiteApplication.java
@@ -2,12 +2,22 @@ package at.gradwohl.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(path = "api/v1")
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
+	}
+
+	@GetMapping
+	public String getTest(){
+		return "Test";
 	}
 
 }
