123a05e00d7c236bdad0e17dd2006fbe86c17bd1
antoschka22 <antoniomolina2207@gmail.com>
2023-09-11 15:43:27 +0200

    JWT Token/Spring security + Endpoints Security

---
+++ backend/spring/src/main/java/at/gradwohl/website/controller/model/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package at.gradwohl.website.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String name;
+    private String password;
+}
---
+++ backend/spring/src/main/java/at/gradwohl/website/controller/model/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package at.gradwohl.website.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
